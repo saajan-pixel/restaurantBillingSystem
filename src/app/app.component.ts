5ea98633e6606d7f7506d8fc7fe01c69b512cf2b
saajan-pixel <72288339+saajan-pixel@users.noreply.github.com>
2024-01-23 15:44:51 +0545

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,8 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     console.log("initial")
     const url = 'https://restaurant-billing-system.vercel.app/';
-    const title = 'change Page Title';
-    const description = 'chnage page description';
+    const title = 'ngoninit Page Title';
+    const description = 'ngoninit page description';
     const image = 'https://www.myhealthunlimited.com/wp-content/uploads/2016/07/300x200.png'
     this.meta.updateTag({ property: 'og:url', content: url });
     this.meta.updateTag({ property: 'og:type', content: 'website' });
@@ -27,7 +27,7 @@ export class AppComponent implements OnInit {
   onClickFb() {
     // Open a new window or redirect to Facebook share URL
 
-    const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent('url')}`;
+    const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent('https://restaurant-billing-system.vercel.app/')}`;
     window.open(facebookShareUrl, '_blank');
   }
 }
