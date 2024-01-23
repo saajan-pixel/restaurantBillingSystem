8548f9b8a41ff6b903edaf754f5ec33c9ccb5fa2
saajan-pixel <72288339+saajan-pixel@users.noreply.github.com>
2024-01-23 15:14:07 +0545

    meta test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'restaurantBillSystem';
+
+  constructor(private meta: Meta) { }
+
+  ngOnInit(): void {
+    console.log("initial")
+    const url = 'https://hrisstagefev2.ndplhrm.com/';
+    const title = 'change Page Title';
+    const description = 'chnage page description';
+    const image = 'https://www.myhealthunlimited.com/wp-content/uploads/2016/07/300x200.png'
+    this.meta.updateTag({ property: 'og:url', content: url });
+    this.meta.updateTag({ property: 'og:type', content: 'website' });
+    this.meta.updateTag({ property: 'og:title', content: title });
+    this.meta.updateTag({ name: 'og:description', content: description });
+    this.meta.updateTag({ property: 'og:image', content: image });
+  }
 }
